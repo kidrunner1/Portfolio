56405ae5f611009148f4452074d75c51ff28a34b
Krit Daowaset <kidrunner1@gmail.com>
2024-08-15 21:32:27 +0700

    Profoliio

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer",
+        "Mobile Developer",
+        "Programer"],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+
+})
